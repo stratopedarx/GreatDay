cd42163aa67864be4fc1a275c4cbfd4cf06d1cbf
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-06-29 22:37:39 +0200

    Use session with custom timeout configurations

--- practice_2/api/services/ApiManager.swift
+++ practice_2/api/services/ApiManager.swift
@@ -12,7 +12,7 @@ enum ApiType {
     }
     var path: String {
         switch self {
-        case .getRandom: return "image/random/6"
+        case .getRandom: return "image/random/\(numberOfImages)"
         }
     }
     var request: URLRequest {
@@ -26,14 +26,23 @@ enum ApiType {
             return request
         }
     }
+    
+    
 }
 
 class ApiManager {
     static let shared = ApiManager()
+
+    var configuration: URLSessionConfiguration {
+        let configuration = URLSessionConfiguration.default
+        configuration.timeoutIntervalForRequest = TimeInterval(15)
+        configuration.timeoutIntervalForResource = TimeInterval(15)
+        return configuration
+    }
     
     func getRandom(completion: @escaping (RandomImage) -> Void) {
         let request = ApiType.getRandom.request
-        let task = URLSession.shared.dataTask(with: request) { data, response, error in
+        let task = URLSession(configuration: configuration).dataTask(with: request) { data, response, error in
             if let data = data, let images = try? JSONDecoder().decode(RandomImage.self, from: data) {
                 completion(images)
             } else {
