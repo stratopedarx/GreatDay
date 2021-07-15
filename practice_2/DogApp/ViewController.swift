c9c64a35bbd6f7cd4a6f4792d5681cfc2feeb886
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-07-15 17:30:27 +0200

    Initial Star Wars app commit. Add Tab Bar Controller. Refactoring.

--- practice_2/DogApp/ViewController.swift
+++ practice_2/DogApp/ViewController.swift
@@ -15,16 +15,19 @@ class ViewController: UIViewController {
         tableView.delegate = self
         tableView.dataSource = self
 
-        ApiManager.shared.getRandom { images in
-            for link in images.message! {
-                self.models.append(Model(imageLink: link))
-            }
-        }
+//        ApiManager.shared.getRandom { images in
+//            for link in images.message! {
+//                self.models.append(Model(imageLink: link))
+//            }
+//        }
 
         // we use test models if we get https errors or not corrected data
-        if models.count % 6 != 0 {
+//        if models.count % 6 != 0 {
+//            createTestModels()
+//        }
+        
+        // TODO: delete it later
             createTestModels()
-        }
     }
 
     private func createTestModels() {
