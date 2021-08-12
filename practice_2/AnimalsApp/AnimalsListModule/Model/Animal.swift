d3eefa298ba56fa1bf4a40929c2c447d057dbfac
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-08-12 06:51:55 +0200

    Add new field to the model to support Cat animals

--- practice_2/AnimalsApp/AnimalsListModule/Model/Animal.swift
+++ practice_2/AnimalsApp/AnimalsListModule/Model/Animal.swift
@@ -1,10 +1,15 @@
 class Animal {
     var breed: String
+    var breedId: String?  // used only for cat
     var imageLink: String
     var isUsed: Bool = false
 
-    init(_ breed: String, _ imageLink: String) {
+    init(breed: String, breedId: String?, imageLink: String) {
         self.breed = breed.replacingOccurrences(of: "/", with: " ")
-        self.imageLink = imageLink  // e.g. https://images.dog.ceo/breeds/bulldog-boston/n02096585_318.jpg
+        self.breedId = breedId
+
+        // dog api: https://images.dog.ceo/breeds/bulldog-boston/n02096585_318.jpg
+        // cat api: https://cdn2.thecatapi.com/images/KWdLHmOqc.jpg
+        self.imageLink = imageLink
     }
 }
