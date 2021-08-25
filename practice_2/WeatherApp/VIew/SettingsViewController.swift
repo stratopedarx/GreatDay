84e6164d07e5d52afaba4a7470dcfcdd5b19c0a1
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-08-25 06:33:03 +0200

    Fix a bug with default Selected segment control Index.

--- practice_2/WeatherApp/VIew/SettingsViewController.swift
+++ practice_2/WeatherApp/VIew/SettingsViewController.swift
@@ -1,11 +1,32 @@
 import UIKit
 import Foundation
 
+enum SegmentIndex: Int {
+    case metric = 0
+    case imperial = 1
+}
+
 class SettingsViewController: UIViewController {
+
+    @IBOutlet weak var unitSegment: UISegmentedControl!
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        setInitialSegmentIndex()
     }
+
+    private func setInitialSegmentIndex() {
+        if let unit = UserDefaults.standard.string(forKey: "temperutureUnit") {
+            if unit == "°C" {
+                unitSegment.selectedSegmentIndex = SegmentIndex.metric.rawValue
+            } else {
+                unitSegment.selectedSegmentIndex = SegmentIndex.imperial.rawValue
+            }
+        } else {
+            return unitSegment.selectedSegmentIndex = SegmentIndex.metric.rawValue
+        }
+    }
+
     @IBAction func unitChanged(_ sender: UISegmentedControl) {
         if let unit = sender.titleForSegment(at: sender.selectedSegmentIndex) {
             UserDefaults.standard.set(unit, forKey: "temperutureUnit")
