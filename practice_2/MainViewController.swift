56b40d6e7ae756073d8d89bed14c00ff61887cf3
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-09-13 20:35:50 +0200

    WIP: Adding slide menu.

---
+++ practice_2/MainViewController.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+class MainViewController: UIViewController {
+    @IBOutlet private weak var tabbarContainerView: UIView!
+    @IBOutlet private weak var slideMenuView: UIView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+}
