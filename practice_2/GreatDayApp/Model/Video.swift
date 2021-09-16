8610855da391ee28199c1536372778fe23304ac0
Sergey Lobanov <stratopedarx.lobanov@yandex.ru>
2021-09-16 22:07:04 +0200

    Add delete and favorite actions

--- practice_2/GreatDayApp/Model/Video.swift
+++ practice_2/GreatDayApp/Model/Video.swift
@@ -3,6 +3,7 @@ import Foundation
 struct Video {
     var youtubeKey: String
     var title: String
+    var isFavourite: Bool = false
     var urlToImage: String {
         return "https://i.ytimg.com/vi/\(youtubeKey)/hqdefault.jpg"
     }
--- practice_2/GreatDayApp/View/ExercisesViews/ExercisesTableViewController.swift
+++ practice_2/GreatDayApp/View/ExercisesViews/ExercisesTableViewController.swift
@@ -70,6 +70,39 @@ extension ExercisesTableViewController {
             videoVC.video = video
         }
     }
+
+    override func tableView(
+        _ tableView: UITableView,
+        leadingSwipeActionsConfigurationForRowAt indexPath: IndexPath) -> UISwipeActionsConfiguration? {
+        let delete = deleteAction(at: indexPath)
+        let favourite = favouriteAction(at: indexPath)
+        return UISwipeActionsConfiguration(actions: [delete, favourite])
+    }
+
+    func deleteAction(at indexPath: IndexPath) -> UIContextualAction {
+        let action = UIContextualAction(style: .destructive,
+                                        title: "Delete") { (_, _, completion) in
+            self.videos.remove(at: indexPath.row)
+            self.tableView.deleteRows(at: [indexPath], with: .automatic)
+            completion(true)
+        }
+        action.backgroundColor = .systemRed
+        action.image = UIImage(systemName: "xmark.bin.fill")
+        return action
+    }
+
+    func favouriteAction(at indexPath: IndexPath) -> UIContextualAction {
+        var video = videos[indexPath.row]
+        let action = UIContextualAction(style: .normal, title: "Favourite") { _, _, completion in
+            video.isFavourite = !video.isFavourite
+            self.videos[indexPath.row] = video
+            self.changeButtonColor(for: video, at: indexPath)
+            completion(true)
+        }
+        action.backgroundColor = video.isFavourite ? .systemGreen : .systemGray
+        action.image = UIImage(systemName: "heart")
+        return action
+    }
 }
 
 private extension ExercisesTableViewController {
@@ -81,5 +114,15 @@ private extension ExercisesTableViewController {
         let fifthVideo = Video(youtubeKey: "zJNkxuechzY", title: "Утренняя гимнастика (18 минут)")
         let sixthVideo = Video(youtubeKey: "SjJs4VhvMy0", title: "Комплекс упражнений дома (7 минут)")
         videos = [firstVideo, secondVideo, thirdVideo, fourthVideo, fifthVideo, sixthVideo]
+    }
+
+    func changeButtonColor(for video: Video, at indexPath: IndexPath) {
+        if let cell = tableView.cellForRow(at: indexPath) as? ExercisesCell {
+            if video.isFavourite {
+                cell.backgroundColor = .systemGreen
+            } else {
+                cell.backgroundColor = .white
+            }
+        }
     }
 }
